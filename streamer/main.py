aab6aa4a66e6d7bf1003ebfb75520c3048b04d3f
nobbynobbs <rbolkhovitin@gmail.com>
2019-04-22 22:01:53 +0300

    add index page and handler stub

--- streamer/main.py
+++ streamer/main.py
@@ -1,18 +1,64 @@
 #!/usr/bin/env python3
 
+import argparse
+import os
+import re
 
+import aiofiles
 from aiohttp import web
 
 
-async def hello(request):
-    return web.Response(text="Hello, world")
+VALID_FOLDER_NAME = re.compile(r"^[\w\d]{4,}$")
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="aiohttp based streaming service")
+    parser.add_argument(
+        "-s", "--storage", required=True, type=str, help="path to file containing directory"
+    )
+    args = parser.parse_args()
+    return args
+
+
+async def archivate(request):
+    
+    dirname = request.match_info["hash"]  # type: str
+    if not VALID_FOLDER_NAME.match(dirname):
+        raise web.HTTPBadRequest(text="only letters and digits allowed in param")
+        # return web.json_response(
+        #     {"message": "bad request, only letters and digits allowed in param"},
+        #     status=400
+        # )
+    base_path = request.app["storage"]
+    full_path = os.path.join(base_path, dirname)
+    if os.path.isdir(full_path):    
+        return web.Response(text="Hello, {}".format(dirname))
+    raise web.HTTPNotFound(text="folder was deleted or never existed")
+    # return web.json_response(
+    #         {"message": "folder was deleted or never existed"},
+    #         status=404
+    # )
+
+async def handle_index_page(request):
+    async with aiofiles.open('templates/index.html', mode='r') as index_file:
+        index_contents = await index_file.read()
+    return web.Response(text=index_contents, content_type='text/html')
+
+
+def get_app(storage):
+    """prepare and return aiohttp app instance"""
+    app = web.Application(middlewares=[web.normalize_path_middleware()])
+    app["storage"] = storage
+    app.add_routes([
+        web.get("/", handle_index_page),
+        web.get("/archive/{hash}/", archivate),
+
+    ])
+    return app
 
 
 def main():
-    app = web.Application()
-    app.add_routes([
-        web.get("/", hello)
-    ])
+    args = parse_args()
+    app = get_app(args.storage)
     web.run_app(app)
 
 
